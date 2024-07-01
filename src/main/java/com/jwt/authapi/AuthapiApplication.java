76642ed7eb2f0ccc1ebcaf5e25be4176d0e1889f
Surith <surithcodes204@gmail.com>
2024-07-01 16:17:45 +0530

    init

---
+++ src/main/java/com/jwt/authapi/AuthapiApplication.java
@@ -0,0 +1,13 @@
+package com.jwt.authapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jwt/authapi/AuthapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jwt.authapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
