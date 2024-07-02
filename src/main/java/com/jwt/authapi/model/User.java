4c93df95a37ab208ee9252dbdb965a50deb098b4
Surith <surithcodes204@gmail.com>
2024-07-02 12:39:50 +0530

    congig

--- src/main/java/com/jwt/authapi/model/User.java
+++ src/main/java/com/jwt/authapi/model/User.java
@@ -1,9 +1,13 @@
 package com.jwt.authapi.model;
 
 import java.util.Date;
+import java.util.List;
+import java.util.Collection;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -14,10 +18,11 @@ import jakarta.persistence.GenerationType;
 
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements UserDetails{
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false)
     private Integer id;
 
     @Column(nullable = false)
@@ -36,6 +41,11 @@ public class User {
     @UpdateTimestamp
     @Column(name = "Updated_at")
     private Date updatedAt;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities(){
+        return List.of();
+    }
 
     //setting up the getters and setters.
     public Integer getId() {
@@ -76,6 +86,31 @@ public class User {
 
     public Date getUpdatedAt() {
         return updatedAt;
+    }
+
+    @Override
+    public String getUsername(){
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired(){
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked(){
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired(){
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled(){
+        return true;
     }
 }
 
--- src/main/java/com/jwt/authapi/repository/UserRepository.java
+++ src/main/java/com/jwt/authapi/repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.jwt.authapi.repository;
 
 import com.jwt.authapi.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.Optional;
 
-
-public interface user_repository extends JpaRepository<User, Integer>{
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer>{
     Optional<User> findByEmail(String email);
 }
